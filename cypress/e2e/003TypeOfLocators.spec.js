a33c8e98f2c8181b599ad27eb051fb3c0c8e16c8
Juliano Peres <juliano.a.peres@gmail.com>
2024-07-31 07:25:54 -0300

    test

---
+++ cypress/e2e/003TypeOfLocators.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+
+
+describe('Finding element', () => {
+
+  it('Finding elements', () => {
+
+    cy.visit('/') // Navigate to the web application
+
+    cy.contains('Forms').click()  // Find element  by text and click on it
+
+    cy.contains('Form Layouts').click()
+
+    cy.get('input') // By Tag Name. "input" is the name of the tag
+
+    cy.get('#inputEmail') // By ID. "inputEmail", is id and has to start with "#"
+
+    cy.get('.input-full-width') // By Class value. "input-full-width" is the class value and has to start with "."
+
+    cy.get('[fullwidth]') //By attribute name."fullwidth" is the attribute name and has to be between square braces
+
+    cy.get('[placeholder="Email"]') // By attribute and value.
+
+    cy.get('[class="input-full-width size-medium shape-rectangle"]')  // By entire class value. has to be between square braces
+
+    cy.get('[placeholder="Email"][fullwidth]') // By two Attributes
+
+    cy.get('input[placeholder="Email"]#inputEmail.input-full-width') // By tag, attribute id and class
+
+    cy.get('[data-cy="imputEmail1"]') // By cypress test ID
+
+  })
+
+})
